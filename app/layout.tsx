e843591cd9e9be45ef30b67dfb06c85ad7844e20
JP <jonathan.branski96@gmail.com>
2025-11-18 19:38:12 -0600

    adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ADSENSE */}
+        <script>
+          <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE}`}
+            crossOrigin="anonymous"></script>
+        </script>
+        {/* ANALYTICS */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS}');
+    `,
+          }}
+        />
+
+
+      </head>
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
